a8ab761053712c32cb544d4a86b1f41ee1ba99a5
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-10-20 16:55:47 -0200

    More box testing

--- sudokugen/src/test/java/com/hammerox/sudokugen/BoxTest.java
+++ sudokugen/src/test/java/com/hammerox/sudokugen/BoxTest.java
@@ -13,21 +13,33 @@ public class BoxTest {
     public void positionBoxRelation() {
         byte[] box = Box.getBoxIndexes(2, 3);
         Assert.assertArrayEquals(box, Box.BOX_TOP_LEFT);
+        byte[] indexBox = Box.getBoxIndexes(0);
+        Assert.assertArrayEquals(indexBox, Box.BOX_TOP_LEFT);
 
         box = Box.getBoxIndexes(4, 3);
         Assert.assertArrayEquals(box, Box.BOX_CENTER_LEFT);
+        indexBox = Box.getBoxIndexes(29);
+        Assert.assertArrayEquals(indexBox, Box.BOX_CENTER_LEFT);
 
         box = Box.getBoxIndexes(9, 2);
         Assert.assertArrayEquals(box, Box.BOX_BOTTOM_LEFT);
+        indexBox = Box.getBoxIndexes(73);
+        Assert.assertArrayEquals(indexBox, Box.BOX_BOTTOM_LEFT);
 
         box = Box.getBoxIndexes(2, 4);
         Assert.assertArrayEquals(box, Box.BOX_TOP_CENTER);
+        indexBox = Box.getBoxIndexes(12);
+        Assert.assertArrayEquals(indexBox, Box.BOX_TOP_CENTER);
 
         box = Box.getBoxIndexes(4, 5);
         Assert.assertArrayEquals(box, Box.BOX_CENTER_CENTER);
+        indexBox = Box.getBoxIndexes(31);
+        Assert.assertArrayEquals(indexBox, Box.BOX_CENTER_CENTER);
 
         box = Box.getBoxIndexes(9, 6);
         Assert.assertArrayEquals(box, Box.BOX_BOTTOM_CENTER);
+        indexBox = Box.getBoxIndexes(77);
+        Assert.assertArrayEquals(indexBox, Box.BOX_BOTTOM_CENTER);
     }
 
 }
