7e0d74aaf96dafb03c1e85bf2a26f3244eb1b897
Mauricio Custodio <mauricio_rc2@hotmail.com>
2017-01-13 04:17:43 -0200

    Debugged: Sidebutton's icon was not showing on API21+. Added elevation to fix

--- app/src/main/java/com/example/hammerox/oxsudoku/ui/views/SudokuKeyboard.java
+++ app/src/main/java/com/example/hammerox/oxsudoku/ui/views/SudokuKeyboard.java
@@ -10,7 +10,6 @@ import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import com.example.hammerox.oxsudoku.R;
 import com.example.hammerox.oxsudoku.utils.GameTools;
