bcac7c6f813442e931f87fe8be948b6f2b7978e8
Mauricio Custodio <mauricio_rc2@hotmail.com>
2017-02-02 17:44:14 -0200

    Created Position class

--- sudokugen/src/main/java/com/hammerox/sudokugen/Cell.java
+++ sudokugen/src/main/java/com/hammerox/sudokugen/Cell.java
@@ -7,14 +7,22 @@ package com.hammerox.sudokugen;
 public class Cell {
 
     public final int index;
+    public final Position position;
     public final int row;
     public final int col;
     public final GridPosition.Box box;
 
+
     public Cell(int row, int col) {
         this.row = row;
         this.col = col;
+        this.position = new Position(row, col);
         this.index = GridPosition.getIndex(row, col);
         this.box = GridPosition.getBox(row, col);
     }
+
+    public Cell(Position position) {
+        this(position.row, position.col);
+    }
+
 }
--- sudokugen/src/test/java/com/hammerox/sudokugen/CellTest.java
+++ sudokugen/src/test/java/com/hammerox/sudokugen/CellTest.java
@@ -10,10 +10,20 @@ import org.junit.Test;
 public class CellTest  {
 
     @Test
-    public void createCell() {
+    public void shouldCreateCellWithIntegers() {
         int row = 1;
         int col = 1;
         Cell cell = new Cell(row, col);
+        Assert.assertEquals(0, cell.index);
+        Assert.assertEquals(1, cell.row);
+        Assert.assertEquals(1, cell.col);
+        Assert.assertEquals(GridPosition.Box.TOP_LEFT, cell.box);
+    }
+
+    @Test
+    public void shouldCreateCellWithPosition() {
+        Position position = new Position(2, 3);
+        Cell cell = new Cell(position);
         Assert.assertEquals(0, cell.index);
         Assert.assertEquals(1, cell.row);
         Assert.assertEquals(1, cell.col);
