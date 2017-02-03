c537e953fcbf759503fc7212331dfc3d426eebb4
Mauricio Custodio <mauricio_rc2@hotmail.com>
2017-02-03 19:51:48 -0200

    Addede reach to cells

--- sudokugen/src/main/java/com/hammerox/sudokugen/Cell.java
+++ sudokugen/src/main/java/com/hammerox/sudokugen/Cell.java
@@ -1,5 +1,7 @@
 package com.hammerox.sudokugen;
 
+import java.util.Set;
+
 /**
  * Created by Mauricio on 02-Feb-17.
  */
@@ -11,6 +13,7 @@ public class Cell {
     public final int row;
     public final int col;
     public final GridPosition.Box box;
+    public final Set<Integer> reach;
 
 
     public Cell(int row, int col) {
@@ -19,6 +22,7 @@ public class Cell {
         this.position = new Position(row, col);
         this.index = GridPosition.getIndex(row, col);
         this.box = GridPosition.getBox(row, col);
+        this.reach = GridPosition.getReachedIndexes(position, false);
     }
 
     public Cell(Position position) {
