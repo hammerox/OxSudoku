43c45b299c40293e16f81af5a53aed4154e49a49
Mauricio Custodio <mauricio_rc2@hotmail.com>
2017-02-03 21:41:47 -0200

    Grid now initialize 81 cells on constructor

--- sudokugen/src/main/java/com/hammerox/sudokugen/Grid.java
+++ sudokugen/src/main/java/com/hammerox/sudokugen/Grid.java
@@ -1,138 +1,40 @@
 package com.hammerox.sudokugen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Mauricio on 20-Oct-16.
  */
 public class Grid {
 
-    private byte[] indexToRow;
-    private byte[] indexToCol;
-    private byte[] indexToBox;
-
-    private byte[][] rowToIndex;
-    private byte[][] colToIndex;
-    private byte[][] boxToIndex;
+    private List<Cell> cells;
 
 
-    public Grid() {}
-
-
-    //
-    // Getters and array inicialization
-    //
-
-
-    public byte[] getIndexToRow() {
-        if (indexToRow == null) {
-            createIndexToRow();
-        }
-        return indexToRow;
-    }
-
-    public byte[] getIndexToCol() {
-        if (indexToCol == null) {
-            createIndexToCol();
-        }
-        return indexToCol;
+    public Grid() {
+        initializeGrid();
     }
 
 
-    public byte[] getIndexToBox() {
-        if (indexToBox == null) {
-            createIndexToBox();
-        }
-        return indexToBox;
+    private Cell getCell(int index) {
+        return cells.get(index);
     }
 
-    public byte[][] getRowGrid() {
-        if (rowToIndex == null) {
-            createRowGrid();
-        }
-        return rowToIndex;
+    private Cell getCell(Position position) {
+        int index = GridPosition.getIndex(position);
+        return getCell(index);
     }
 
-    public byte[][] getColGrid() {
-        if (colToIndex == null) {
-            createColGrid();
-        }
-        return colToIndex;
+    private Cell getCell(int row, int col) {
+        int index = GridPosition.getIndex(row, col);
+        return getCell(index);
     }
 
-    public byte[][] getBoxGrid() {
-        if (boxToIndex == null) {
-            boxToIndex = new byte[9][9];
-            createFirstBox();
-            createOtherBoxes();
-        }
-        return boxToIndex;
-    }
-
-    //
-    // Private Methods
-    //
-
-    private void createIndexToRow() {
-        indexToRow = new byte[81];
-        for (int index = 0; index < 81; index++) {
-            indexToRow[index] = (byte) (index / 9);
-        }
-    }
-
-    private void createIndexToCol() {
-        indexToCol = new byte[81];
-        for (int index = 0; index < 81; index++) {
-            indexToCol[index] = (byte) (index % 9);
-        }
-    }
-
-    private void createIndexToBox() {
-        indexToBox = new byte[81];
-        boxToIndex = getBoxGrid();
-        for (int box = 0; box < 9; box++) {
-            for (int j = 0; j < 9; j++) {
-                indexToBox[ boxToIndex[box][j] ] = (byte) box;
-            }
-        }
-    }
-
-    private void createRowGrid() {
-        rowToIndex = new byte[9][9];
-        byte index = 0;
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                rowToIndex[row][col] = index++;
-            }
-        }
-    }
-
-    private void createColGrid() {
-        colToIndex = new byte[9][9];
-        byte index = 0;
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                colToIndex[col][row] = index++;
-            }
-        }
-    }
-
-    private void createFirstBox() {
-        boxToIndex[0][0] = 0;
-        int sumIndex = 0;
-        int[] sumArray = new int[] {1, 1, 7};
-        for (int j = 1; j < 9; j++) {
-            boxToIndex[0][j] = (byte) (boxToIndex[0][j-1] + sumArray[sumIndex]);
-            sumIndex = sumIndex == 2 ? 0 : ++sumIndex;
-        }
-    }
-
-    private void createOtherBoxes() {
-        byte[] sumArray = new byte[] {3, 3, 21};
-        for (int j = 0; j < 9; j++) {
-            int sumIndex = 0;
-            for (int box = 1; box < 9; box++) {
-                boxToIndex[box][j] = (byte) (boxToIndex[box - 1][j] + sumArray[sumIndex]);
-                sumIndex = sumIndex == 2 ? 0 : ++sumIndex;
-            }
+    private void initializeGrid() {
+        cells = new ArrayList<>();
+        for (int i = 0; i < 81; i++) {
+            Position position = GridPosition.getPosition(i);
+            cells.add(new Cell(position));
         }
     }
 
