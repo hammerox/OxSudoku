beae613caf0ee3c72d9f7cac40d4722dc364aca5
Mauricio Custodio <mauricio_rc2@hotmail.com>
2017-02-03 22:23:22 -0200

    Fixed main function

--- sudokugen/src/main/java/com/hammerox/sudokugen/JavaTest.java
+++ sudokugen/src/main/java/com/hammerox/sudokugen/JavaTest.java
@@ -8,49 +8,7 @@ public class JavaTest {
 
     public static void main(String[] args) {
         System.out.printf("helloworld\n");
-
-        printRowGrid();
-        printColGrid();
-        printBoxGrid();
     }
 
-
-    public static void printRowGrid() {
-        byte[][] grid = new Board().getRowGrid();
-
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                System.out.format("%2d ", grid[row][col]);
-            }
-            System.out.print("\n");
-        }
-        System.out.print("\n");
-    }
-
-
-    public static void printColGrid() {
-        byte[][] grid = new Board().getColGrid();
-
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                System.out.format("%2d ", grid[row][col]);
-            }
-            System.out.print("\n");
-        }
-        System.out.print("\n");
-    }
-
-
-    public static void printBoxGrid() {
-        byte[][] grid = new Board().getBoxGrid();
-
-        for (int box = 0; box < 9; box++) {
-            for (int item = 0; item < 9; item++) {
-                System.out.format("%2d ", grid[box][item]);
-            }
-            System.out.print("\n");
-        }
-        System.out.print("\n");
-    }
 
 }
